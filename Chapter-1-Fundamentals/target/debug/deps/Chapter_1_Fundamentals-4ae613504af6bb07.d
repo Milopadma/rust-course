0dde2456c5c2df6456eb56ba6bf9f21c68271c8d
mylo <milopadma@yahoo.com>
2023-01-18 22:38:19 +0800

    init

---
+++ Chapter-1-Fundamentals/target/debug/deps/Chapter_1_Fundamentals-4ae613504af6bb07.d
@@ -0,0 +1,5 @@
+/Users/mylo/Code/rust-course/Chapter-1-Fundamentals/target/debug/deps/Chapter_1_Fundamentals-4ae613504af6bb07.rmeta: src/main.rs
+
+/Users/mylo/Code/rust-course/Chapter-1-Fundamentals/target/debug/deps/Chapter_1_Fundamentals-4ae613504af6bb07.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter-1-Fundamentals/target/debug/deps/Chapter_1_Fundamentals-6aab871993082600.d
@@ -0,0 +1,5 @@
+/Users/mylo/Code/rust-course/Chapter-1-Fundamentals/target/debug/deps/Chapter_1_Fundamentals-6aab871993082600.rmeta: src/main.rs
+
+/Users/mylo/Code/rust-course/Chapter-1-Fundamentals/target/debug/deps/Chapter_1_Fundamentals-6aab871993082600.d: src/main.rs
+
+src/main.rs:
