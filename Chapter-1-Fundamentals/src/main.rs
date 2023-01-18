5ed510fd44eb7d81987877e5f61530cf1e3342a9
mylo <milopadma@yahoo.com>
2023-01-18 23:06:44 +0800

    leson4

--- Chapter-1-Fundamentals/src/main.rs
+++ Chapter-1-Fundamentals/src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
     println!("wow!");
     println!("this chapter is mostly the basics about data types, variables and functions...");
+
+    // Variables
+    let x = 5;
+    let y = 2;
+
+    // Data types
+    let a: i32 = 5;
+    let b: i32 = 2;
+    let c: f64 = 5.0;
+    let d: f64 = 2.0;
+
+    // Functions
+    println!("{} + {} = {}", x, y, add(x, y));
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
