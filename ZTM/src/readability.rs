046197a70286b56950bf85194628410a44cc1c49
mylo <milopadma@yahoo.com>
2023-02-22 19:19:29 +0800

    actual

--- ZTM/src/readability.rs
+++ ZTM/src/readability.rs
@@ -1,55 +1,61 @@
 mod typestates {
     // the multiple states for luggage airport process
-    struct CheckIn;
-    struct OnLoading;
-    struct Offloading;
-    struct AwaitingPickup;
-    #[derive(Debug)]
-    struct EndCustody;
+    struct CheckIn(LuggageID);
+    struct OnLoading(LuggageID);
+    struct Offloadin(LuggageID);
+    struct AwaitingPickup(LuggageID);
+    struct EndCustody(LuggageID);
 
     // the luggage struct
-    struct Luggage<T> {
-        state: T,
-    }
+    struct LuggageID(usize);
+    struct Luggage(LuggageID);
 
     // the luggage struct with a method to change state
-    impl<T> Luggage<T> {
-        fn new(state: T) -> Self {
-            Luggage { state }
+    impl Luggage {
+        fn new(id: LuggageID) -> Self {
+            Luggage(id)
         }
 
         fn check_in(self) -> Luggage<CheckIn> {
-            Luggage { state: CheckIn }
+            Luggage(CheckIn(self.0))
         }
+    }
 
-        fn on_loading(self) -> Luggage<OnLoading> {
-            Luggage { state: OnLoading }
+    // as these types will only be able to run said functions
+    impl CheckIn {
+        fn onload(self) -> OnLoad {
+            OnLoad(self.0)
         }
+    }
 
-        fn offloading(self) -> Luggage<Offloading> {
-            Luggage { state: Offloading }
+    impl OnLoading {
+        fn offload(self) -> Offload {
+            Offload(self.0)
         }
+    }
 
-        fn awaiting_pickup(self) -> Luggage<AwaitingPickup> {
-            Luggage {
-                state: AwaitingPickup,
-            }
+    impl Offload {
+        fn awaiting_pickup(self) -> AwaitingPickup {
+            AwaitingPickup(self.0)
         }
+    }
 
-        fn end_custody(self) -> Luggage<EndCustody> {
-            Luggage { state: EndCustody }
+    impl AwaitingPickup {
+        fn pickup(self) -> (Luggage, EndCustody) {
+            (Luggage(self.0), EndCustody(self.0))
         }
     }
 
     // run
     pub fn run() {
-        let luggage = Luggage::new(CheckIn);
-        let luggage = luggage.check_in();
-        let luggage = luggage.on_loading();
-        let luggage = luggage.offloading();
-        let luggage = luggage.awaiting_pickup();
-        let luggage = luggage.end_custody();
-
+        let id = LuggageID(1);
+        // we did all that so we can do this daisy chain
+        let luggage = Luggage::new(id)
+            .check_in()
+            .onload()
+            .offload()
+            .awaiting_pickup()
+            .pickup();
         println!("Luggage state: {:?}", luggage.state);
     }
 }
