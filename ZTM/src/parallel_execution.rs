db0ca3f6866b567766bb688d3e5c0e00344ec5fe
mylo <milopadma@yahoo.com>
2023-02-25 16:54:45 +0800

    parallel execution using closures

--- ZTM/src/parallel_execution.rs
+++ ZTM/src/parallel_execution.rs
@@ -3,7 +3,10 @@ fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
 }
 
 fn main() {
-    let add = |a, b| a + b;
-    let add = Box::new(add);
+    let add = Box::new(|a, b| a + b);
+    let sub = Box::new(|a, b| a - b);
+    let divide = Box::new(|a, b| a / b);
     println!("{}", math(2, 2, add));
+    println!("{}", math(2, 2, sub));
+    println!("{}", math(2, 2, divide));
 }
