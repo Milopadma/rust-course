3e936928d8e219388ac3d4ded1cbe46b911888b6
mylo <milopadma@yahoo.com>
2023-02-25 17:19:32 +0800

    divide by 0 guard

--- ZTM/src/parallel_execution.rs
+++ ZTM/src/parallel_execution.rs
@@ -5,8 +5,9 @@ fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
 fn main() {
     let add = Box::new(|a, b| a + b);
     let sub = Box::new(|a, b| a - b);
-    let divide = Box::new(|a, b| a / b);
+    let divide = Box::new(|a, b| if a <= 0 || b <= 0 { 0 } else { a / b });
     println!("{}", math(2, 2, add));
     println!("{}", math(2, 2, sub));
-    println!("{}", math(2, 2, divide));
+    println!("{}", math(2, 2, divide.clone()));
+    println!("{}", math(2, 0, divide));
 }
