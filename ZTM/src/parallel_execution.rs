d5f4bd379f0cdfa1353f6317d61026c7a1a195d1
mylo <milopadma@yahoo.com>
2023-02-25 18:19:44 +0800

    threads

--- ZTM/src/parallel_execution.rs
+++ ZTM/src/parallel_execution.rs
@@ -1,13 +1,21 @@
-fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
-    op(a, b)
+mod advanced_closures {
+    fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
+        op(a, b)
+    }
+
+    pub fn run() {
+        let add = Box::new(|a, b| a + b);
+        let sub = Box::new(|a, b| a - b);
+        let divide = Box::new(|a, b| if a <= 0 || b <= 0 { 0 } else { a / b });
+        println!("{}", math(2, 2, add));
+        println!("{}", math(2, 2, sub));
+        println!("{}", math(2, 2, divide.clone()));
+        println!("{}", math(2, 0, divide));
+    }
 }
 
+mod threads {}
+
 fn main() {
-    let add = Box::new(|a, b| a + b);
-    let sub = Box::new(|a, b| a - b);
-    let divide = Box::new(|a, b| if a <= 0 || b <= 0 { 0 } else { a / b });
-    println!("{}", math(2, 2, add));
-    println!("{}", math(2, 2, sub));
-    println!("{}", math(2, 2, divide.clone()));
-    println!("{}", math(2, 0, divide));
+    advanced_closures::run();
 }
