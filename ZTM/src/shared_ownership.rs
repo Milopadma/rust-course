0cade5c509325c12af926e2d2ed111346e7c39d3
mylo <milopadma@yahoo.com>
2023-03-04 14:22:48 +0800

    smart pointers activity

--- ZTM/src/shared_ownership.rs
+++ ZTM/src/shared_ownership.rs
@@ -125,23 +125,90 @@ mod smart_pointers_interior_mutability_demo {
 
         // all the staff has access to the orders
         let chef = Chef(Rc::clone(&orders));
-        let wait_staff = WaitStaff(orders.clone());
-        let accounting = Accounting(orders.clone());
+        let wait_staff = WaitStaff(Rc::clone(&orders));
+        let accounting = Accounting(Rc::clone(&orders));
 
         // new order
         let new_order = new_table_order();
 
         {
-            orders.borrow_mut().push(new_table_order());
+            orders.borrow_mut().push(new_order);
         }
 
         dbg!(chef.0.borrow());
+        drop(chef); // chef go home and no longer has access to the orders
+                    // but the wait staff still has access to the orders
         dbg!(wait_staff.0.borrow());
         dbg!(accounting.0.borrow());
     }
 }
 
+mod smart_points_ref_cell_activity {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    #[derive(Debug)]
+    enum Vehicle {
+        Car,
+        Truck,
+    }
+
+    #[derive(Debug, Hash, PartialEq, PartialOrd)]
+    enum Status {
+        Avaliable,
+        Unavailable,
+        Maintenance,
+        Rented,
+    }
+
+    #[derive(Debug)]
+    struct Rentals {
+        vehicle_type: Vehicle,
+        vehicle_VIN: u32,
+        vehicle_status: Status,
+    }
+
+    impl Rentals {
+        fn new(vehicle_type: Vehicle, vehicle_VIN: u32, vehicle_status: Status) -> Self {
+            Rentals {
+                vehicle_type,
+                vehicle_VIN,
+                vehicle_status,
+            }
+        }
+    }
+
+    // wrap the Rentals struct in a Rc<RefCell<Rentals>>
+    type RentalsRefCell = Rc<RefCell<Vec<Rentals>>>;
+    // rc means data exists as long as one copy of it exists
+    // refcell means we can mutate the data inside the struct
+    // "anyone that has access can be modified with a copy of it, can make as many copy as we want"
+
+    struct Corporate(RentalsRefCell);
+
+    struct StoreFront(RentalsRefCell);
+
+    pub fn run() {
+        let new_rentals_main = vec![
+            Rentals::new(Vehicle::Car, 1, Status::Avaliable),
+            Rentals::new(Vehicle::Truck, 2, Status::Avaliable),
+        ];
+
+        // wrap it inside a refcell
+        let vehicles_vec_ref_cell = Rc::new(RefCell::new(new_rentals_main));
+
+        // clone the pointers for the vec ref cell
+        let corporate = Corporate(Rc::clone(&vehicles_vec_ref_cell));
+        let store_front = StoreFront(Rc::clone(&vehicles_vec_ref_cell));
+
+        dbg!(new_corporate.0.borrow());
+        drop(new_corporate);
+        dbg!(new_storefront.0.borrow());
+    }
+}
+
 fn main() {
-    shared_mutability::run();
-    shared_mutability_ref_cells::run();
+    // shared_mutability::run();
+    // shared_mutability_ref_cells::run();
+    smart_pointers_interior_mutability_demo::run();
 }
