3ce671c6cfbd8d2a73aa3c67e8cd94d15791e12b
mylo <milopadma@yahoo.com>
2023-03-04 16:31:48 +0800

    mutexes, threads, and deadlocks

--- ZTM/src/shared_ownership.rs
+++ ZTM/src/shared_ownership.rs
@@ -207,6 +207,156 @@ mod smart_points_ref_cell_activity {
     }
 }
 
+mod threads_mutex {
+    // threads can execute at random times, and multiple threads can execute on
+    // the same data, causing the data to become corrupted easily
+
+    // to avoid this, we can use a mutex by data syncronization
+    use std::sync::Mutex; //mutually exclusive lock
+
+    // this ensures that only one thread can access the data at a time
+    // mutexes wrap the data and make the data mutually exclusive
+    // only allow one thread at a time
+
+    // to allow mutexes to be shared among theads, an Arc (atomic reference
+    // count) pointers
+    // is used
+    use std::sync::Arc;
+
+    use parking_lot::Mutex as parking_lot_mutex; // using the parking_lot crate for their mutex
+    use std::thread;
+
+    struct Counter(usize);
+
+    pub fn run() {
+        let counter = Counter(0);
+        // wrap the Mutex wrapped Counter data in an Arc
+        let shared_counter = Arc::new(parking_lot_mutex::new(counter));
+        // two copies of the counter
+        let thread_1_counter = Arc::clone(&shared_counter);
+        let thread_2_counter = shared_counter.clone();
+
+        // utilize them in threads
+        // thread_1  locks the counter
+        let thread_1 = thread::spawn(move || {
+            let mut counter = thread_1_counter.lock();
+            counter.0 += 1;
+        });
+
+        // thread_2 locks the counter
+        let thread_2 = thread::spawn(move || {
+            let mut counter = thread_2_counter.lock();
+            counter.0 += 1;
+        });
+
+        // rejoin thread 1 into main thread and join thread 2
+        thread_1.join().and_then(|_| thread_2.join());
+        println!("{}", shared_counter.lock().0); // this will print out "2"
+    }
+}
+
+mod threading_deadlocks {
+    // a situation where threads are waiting for one another and get stuck
+    // this can happen when :
+    // - using multiple locks
+    // - recursing when taking a lock
+    // - locking the same lock twice
+
+    use parking_lot::Mutex;
+    use std::rc::Rc;
+
+    // recursive function that locks the data it receives,
+    // then keeps recursing until remaining is 0
+    // this is a deadlock
+    fn recurse(data: Rc<Mutex<i32>>, remaining: usize) -> usize {
+        let mut locked = data.lock();
+        match remaining {
+            rem if rem == 0 => 0,
+            rem => recurse(Rc::clone(&data), remaining - 1),
+        }
+    }
+    // can replace mutex with a ReentrantMutex to fix this deadlock
+    // this works because the function is called in the same thread
+}
+
+mod thread_deadlock_example_2 {
+    use parking_lot::Mutex;
+    use std::sync::Arc;
+    use std::thread;
+
+    struct Account {
+        balance: i64,
+    }
+
+    type ArcAccount = Arc<Mutex<Account>>;
+
+    pub fn transfer(from: ArcAccount, to: ArcAccount, amount: i64) {
+        // exclusive access to both accounts data
+        let mut from = from.lock();
+        let mut to = to.lock();
+        from.balance -= amount;
+        to.balance += amount;
+    }
+
+    pub fn run() {
+        // init the ArcAccounts
+        let a = Arc::new(Mutex::new(Account { balance: 0 }));
+        let b = Arc::new(Mutex::new(Account { balance: 0 }));
+
+        // perform transfers using threads
+        let t1 = thread::spawn(move || transfer(a, b, 500));
+        let t2 = thread::spawn(move || transfer(b, a, 500));
+        // this is a deadlock where  both threads can lock at the same time
+        // can fix by retry on failure using Some and try_locks(), and waits
+        // this can cause thread contention/backoff problems though
+
+        // to truly fix this problem, we can use a backoff::ExponentialBackoff
+        // basically using a variable wait times
+    }
+}
+
+mod mutex_demo {
+    use parking_lot::Mutex;
+    use std::sync::Arc;
+    use std::thread;
+    use std::time::Duration;
+
+    type SharedSignData = Arc<Mutex<String>>;
+
+    struct DigitalSignBoard {
+        display: SharedSignData,
+    }
+
+    impl DigitalSignBoard {
+        fn refresh(&self) {
+            let mut locked = self.display.lock();
+
+            // print
+            println!("{}", locked);
+        }
+    }
+
+    fn spawn_display_thread(display_data: SharedSignData) {
+        thread::spawn(move || {
+            let board = DigitalSignBoard {
+                display: display_data,
+            };
+            loop {
+                board.refresh();
+                thread::sleep(Duration::from_millis(1000));
+            }
+        });
+    }
+
+    fn change_data(display_data: SharedSignData, new_data: &str) {
+        let mut data = display_data.lock();
+        *data = new_data.to_string();
+
+        // update self display text
+        display_data = Arc::new(Mutex::new(new_data.to_string()));
+    }
+}
+
 fn main() {
     // shared_mutability::run();
     // shared_mutability_ref_cells::run();
