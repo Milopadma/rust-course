240f27bfb7a1a201e0aa6d72364d3f3656768248
mylo <milopadma@yahoo.com>
2023-03-09 19:18:41 +0800

    project init

---
+++ ZTM_ClipStash_Project/src/lib/data/domain/clip/mod.rs
@@ -0,0 +1,13 @@
+pub mod field;
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Clip {
+    pub name: field::ClipID;
+    pub shortcode: field,
+    pub content: field,
+    pub title: field,
+    pub posted: field,
+    pub expires: field,
+    pub password: field,
+    pub hits: field,
+}
