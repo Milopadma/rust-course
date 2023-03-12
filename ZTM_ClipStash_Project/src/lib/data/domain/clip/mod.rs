0896b4ef747fbba7a76a9dc836bbafd0555d5604
mylo <milopadma@yahoo.com>
2023-03-12 14:47:33 +0800

    error msgs

--- ZTM_ClipStash_Project/src/lib/data/domain/clip/mod.rs
+++ ZTM_ClipStash_Project/src/lib/data/domain/clip/mod.rs
@@ -1,5 +1,26 @@
 pub mod field;
 
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Debug, Error, Serialize, Deserialize)]
+pub enum ClipError {
+    #[error("Invalid passwrod: {0}")]
+    InvalidPassword(String),
+    #[error("Invalid title: {0}")]
+    InvalidTitle(String),
+    #[error("Empty Content")]
+    EmptyContent,
+    #[error("Invalid date: {0}")]
+    InvalidDate(String),
+    #[error("Date parse error: {0}")]
+    DataParse(#[from] chrono::ParseError),
+    #[error("Invalid id: {0}")]
+    Id(#[from] uuid::Error),
+    #[error("Parse int error: {0}")]
+    Hits(#[from] std::num::TryFromIntError),
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Clip {
     pub name: field::ClipId,
