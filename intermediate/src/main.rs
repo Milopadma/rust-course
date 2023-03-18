592d768e256f2b8b24998a8003dde27963c06d7c
mylo <milopadma@yahoo.com>
2023-03-18 16:58:18 +0800

    traits

--- intermediate/src/main.rs
+++ intermediate/src/main.rs
@@ -72,13 +72,14 @@ mod closures_iterators_activity {
 }
 
 mod traits {
-    #[derive(Debug)]
+    #[derive(Eq, PartialEq, Debug, Copy, Clone)]
     pub enum Cake {
         Chocolate,
         MapleBacon,
         Spice,
     }
 
+    #[derive(Debug, Copy, Clone)]
     pub struct Party {
         pub at_restaurant: bool,
         pub num_people: u8,
@@ -92,75 +93,108 @@ mod traits {
         let cake = Cake::Spice;
         admire_cake(cake);
 
-        // 2. Uncomment the code below. It doesn't work since `cake` was *moved* into the admire_cake()
-        // function. Let's fix the Cake enum so the code below works without any changes.
-        // - Derive the Copy trait for the Cake enum so that `cake` gets copied into the admire_cake()
-        // function instead of moved.
-        // - Hint: You may need to derive another trait in order to be able to derive the Copy trait
+        // 2. Uncomment the code below. It doesn't work since `cake` was *moved*
+        // into the admire_cake() function. Let's fix the Cake enum so the code
+        // below works without any changes.
+        // - Derive the Copy trait for the Cake enum so that `cake` gets copied
+        // into the admire_cake() function instead of moved.
+        // - Hint: You may need to derive another trait in order to be able to
+        //   derive the Copy trait
 
-        // match cake {
-        //     Cake::Chocolate => println!("The name's Chocolate. Dark...Chocolate."),
-        //     Cake::MapleBacon => println!("Dreams do come true!"),
-        //     Cake::Spice => println!("Great, let's spice it up!"),
-        // }
+        match cake {
+            Cake::Chocolate => println!("The name's Chocolate. Dark...Chocolate."),
+            Cake::MapleBacon => println!("Dreams do come true!"),
+            Cake::Spice => println!("Great, let's spice it up!"),
+        }
 
-        // 3. Uncomment the println below. It doesn't work since the Party struct doesn't implement the
-        // Debug or Default traits.
+        // 3. Uncomment the println below. It doesn't work since the Party
+        // struct doesn't implement the Debug or Default traits.
         // - Derive the Debug trait for the Party struct
-        // - Manually implement the Default trait for the Party struct. Use the value below as the
-        // default value that you return from the `default` method:
+        // - Manually implement the Default trait for the Party struct. Use the
+        // value below as the default value that you return from the `default`
+        // method:
         //
-        //     Party {
-        //         at_restaurant: true,
-        //         num_people: 8,
-        //         cake: Cake::Chocolate,
-        //     }
+
+        impl Default for Party {
+            fn default() -> Self {
+                Party {
+                    at_restaurant: true,
+                    num_people: 8,
+                    cake: Cake::Chocolate,
+                }
+            }
+        }
         //
         // Hint: If you get stuck, there is an example at
         // https://doc.rust-lang.org/std/default/trait.Default.html#how-can-i-implement-default
 
-        // println!("The default Party is\n{:#?}", Party::default());
+        println!("The default Party is\n{:#?}", Party::default());
 
-        // 4. You prefer Maple Bacon cake. Use "struct update syntax" to create a Party with `cake`
-        // set to `Cake::MapleBacon`, but the rest of the values are default.
+        // 4. You prefer Maple Bacon cake. Use "struct update syntax" to create
+        // a Party with `cake` set to `Cake::MapleBacon`, but the rest of the
+        // values are default.
         //
-        // Hint: The trick to struct update syntax is specifying the value(s) you want to customize
-        // first and then ending the struct with `..Default::default()` -- but no comma after that!
+        // Hint: The trick to struct update syntax is specifying the value(s)
+        // you want to customize first and then ending the struct with
+        // `..Default::default()` -- but no comma after that!
 
-        // let party = Party {
-        //     ...
-        // };
-        // println!("Yes! My party has my favorite {:?} cake!", party.cake);
+        let party = Party {
+            cake: Cake::MapleBacon,
+            ..Default::default()
+        };
+        println!("Yes! My party has my favorite {:?} cake!", party.cake);
 
         // 5. Parties are "equal" if they have the same cake.
-        // - Derive the PartialEq trait for the Cake enum so Cakes can be compared.
-        // - Manually implement the PartialEq trait for Party. If different parties have the same cake,
-        // then they are equal, no matter the location or number of attendees at the party.
+        // - Derive the PartialEq trait for the Cake enum so Cakes can be
+        //   compared.
+        // - Manually implement the PartialEq trait for Party. If different
+        // parties have the same cake, then they are equal, no matter the
+        // location or number of attendees at the party.
         // - Uncomment and run the code below.
 
-        // let other_party = Party {
-        //     at_restaurant: false,
-        //     num_people: 235,
-        //     cake: Cake::MapleBacon,
-        // };
-        // if party == other_party {
-        //     println!("Your party is just like mine!");
-        // }
+        impl PartialEq for Party {
+            fn eq(&self, other: &Self) -> bool {
+                self.cake == other.cake
+            }
+        }
 
-        // Challenge: You would like to be able to pass a Party struct into the smell_cake() function
-        // which takes a type T which implements the Into<Cake> trait.
-        // - Uncomment the code below AND uncomment the smell_cake() function at the bottom of this file
-        // - Implement `From<Party> for Cake` so that the function call below works.
+        let other_party = Party {
+            at_restaurant: false,
+            num_people: 235,
+            cake: Cake::MapleBacon,
+        };
+        if party == other_party {
+            println!("Your party is just like mine!");
+        }
+
+        // Challenge: You would like to be able to pass a Party struct into the
+        // smell_cake() function which takes a type T which implements the
+        // Into<Cake> trait.
+        // - Uncomment the code below AND uncomment the smell_cake() function at
+        //   the bottom of this file
+        // - Implement `From<Party> for Cake` so that the function call below
+        //   works.
         //
 
-        // smell_cake(party);
+        impl From<Party> for Cake {
+            fn from(party: Party) -> Self {
+                party.cake
+            }
+        }
 
-        // Challenge 2: Implement `From<&Party> for Cake` so that you can smell your cake without
-        // consuming it. Change the code above to pass in a &party. Then uncomment and run the code
-        // below. After all, you want to smell your cake and eat it, too!
+        smell_cake(party);
 
-        // println!("Yum! I'm eating this cake: {:?}. Oops, I dropped it on the floor.", party.cake);
-        // drop(cake);
+        // Challenge 2: Implement `From<&Party> for Cake` so that you can smell
+        // your cake without consuming it. Change the code above to pass in a
+        // &party. Then uncomment and run the code below. After all, you want to
+        // smell your cake and eat it, too!
+
+        println!(
+            "Yum! I'm eating this cake: {:?}. Oops, I dropped it on the
+        floor.",
+            party.cake
+        );
+        drop(cake);
     }
 
     pub fn admire_cake(cake: Cake) {
@@ -175,4 +209,5 @@ mod traits {
 fn main() {
     println!("Hello, world!");
     closures_iterators_activity::run();
+    traits::run();
 }
